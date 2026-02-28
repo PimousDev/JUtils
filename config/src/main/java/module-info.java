c02411924d1f59aa6c068dde5c3aa83f5ef9a131
Xibitol <xibitol@pimous.dev>
2026-02-28 17:34:46 +0100

    Created i18n module for internationalization.

--- config/src/main/java/module-info.java
+++ config/src/main/java/module-info.java
@@ -1,3 +1,5 @@
 module dev.pimous.pu.jutils.config{
 	requires transitive dev.pimous.pu.jutils.base;
+
+	exports dev.pimous.pu.jutils.config;
 }
---
+++ i18n/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module dev.pimous.pu.jutils.i18n{
+	requires transitive dev.pimous.pu.jutils.base;
+	requires dev.pimous.pu.jutils.config;
+
+	exports dev.pimous.pu.jutils.i18n;
+}
