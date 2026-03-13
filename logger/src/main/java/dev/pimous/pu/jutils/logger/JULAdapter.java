e02ab4ce965b4fe4028127cf8ffe7fd7b050fa3f
APG-Gillardeau <axel.pelleray@gillardeau.fr>
2026-03-13 17:43:04 +0100

    Created an adapted for JUL logger.

---
+++ logger/src/main/java/dev/pimous/pu/jutils/logger/JULAdapter.java
@@ -0,0 +1,73 @@
+package dev.pimous.pu.jutils.logger;
+
+import dev.pimous.pu.jutils.base.IntrospectiveStringifier;
+
+public class JULAdapter implements Logger{
+
+	private final java.util.logging.Logger logger;
+
+	public JULAdapter(final java.util.logging.Logger logger){
+		this.logger = logger;
+	}
+
+	// GETTERS
+	@Override
+	public String getName(){ return logger.getName(); }
+	@Override
+	public boolean isLoggable(final Level level){
+		return logger.isLoggable(mapLevel(level));
+	}
+
+	// FUNCTIONS
+	private java.util.logging.Level mapLevel(final Level level){
+		return switch(level){
+			case FATAL, CRITICAL, ERROR -> java.util.logging.Level.SEVERE;
+			case WARNING -> java.util.logging.Level.WARNING;
+			case NOTICE -> java.util.logging.Level.INFO;
+			case INFORMATION -> java.util.logging.Level.CONFIG;
+			case DEBUG -> java.util.logging.Level.FINE;
+			case TRACE -> java.util.logging.Level.FINEST;
+		};
+	}
+
+	public void log(final Level level,
+		final String message, final Object ...arguments
+	){
+		logger.log(mapLevel(level), message, arguments);
+	}
+	public void log(final Level level, final Throwable throwable){
+		logger.log(mapLevel(level), throwable.getMessage(), throwable);
+	}
+	public void log(final Level level,
+		final Throwable throwable,
+		final String message, final Object ...arguments
+	){
+		logger.log(mapLevel(level), message.formatted(arguments), throwable);
+	}
+	public void logC(final Level level, final Class<?> clazz){
+		log(level, IntrospectiveStringifier.fromAll(clazz));
+	}
+	public void logC(final Level level,
+		final Class<?> clazz,
+		final String message, final Object ...arguments
+	){
+		log(level,
+			message.formatted(arguments)
+				+ "\n"
+				+ IntrospectiveStringifier.fromAll(clazz)
+		);
+	}
+	public void logO(final Level level, final Object object){
+		log(level, IntrospectiveStringifier.fromAll(object));
+	}
+	public void logO(final Level level,
+		final Object object,
+		final String message, final Object ...arguments
+	){
+		log(level,
+			message.formatted(arguments)
+				+ "\n"
+				+ IntrospectiveStringifier.fromAll(object)
+		);
+	}
+}
--- logger/src/main/java/module-info.java
+++ logger/src/main/java/module-info.java
@@ -2,5 +2,7 @@ module dev.pimous.pu.jutils.logger{
 	requires transitive dev.pimous.pu.jutils.base;
 	requires dev.pimous.pu.jutils.config;
 
+	requires java.logging;
+
 	exports dev.pimous.pu.jutils.logger;
 }
