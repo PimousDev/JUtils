f67d02fb32105eeffb7adae27e7270bc27f460dc
APG-Gillardeau <axel.pelleray@gillardeau.fr>
2026-03-19 15:32:10 +0100

    Fixed missing javadoc on base module classes.

--- base/src/main/java/dev/pimous/pu/jutils/base/Functions.java
+++ base/src/main/java/dev/pimous/pu/jutils/base/Functions.java
@@ -2,6 +2,10 @@ package dev.pimous.pu.jutils.base;
 
 import java.util.function.Function;
 
+/**
+ * @author APG-Gillardeau
+ * @since 1.0.0
+ */
 public final class Functions{
 
 	private Functions(){}
--- base/src/main/java/dev/pimous/pu/jutils/base/IntrospectiveStringifier.java
+++ base/src/main/java/dev/pimous/pu/jutils/base/IntrospectiveStringifier.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 /**
  * @author APG-Gillardeau
