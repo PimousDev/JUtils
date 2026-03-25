34d74f8144f718edd4afc2576675ccf551f46fa0
APG-Gillardeau <axel.pelleray@gillardeau.fr>
2026-03-25 15:56:11 +0100

    Created a logging stream handler that is auto flushed.

---
+++ app/src/main/java/dev/pimous/pu/jutils/app/util/AutoFlushStreamHandler.java
@@ -0,0 +1,23 @@
+package dev.pimous.pu.jutils.app.util;
+
+import java.io.PrintStream;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+import java.util.logging.StreamHandler;
+
+public class AutoFlushStreamHandler extends StreamHandler{
+
+	public AutoFlushStreamHandler(
+		final PrintStream stream,
+		final Formatter formatter
+	){
+		super(stream, formatter);
+	}
+
+	// GETTERS
+	@Override
+	public void publish(LogRecord record){
+		super.publish(record);
+		super.flush();
+	}
+}
